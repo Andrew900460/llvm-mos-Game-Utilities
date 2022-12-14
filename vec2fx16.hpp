220cd02a0bae998df59c22fa77fe132209fdbef0
Andrew900460 <Andrew900460@outlook.com>
2022-12-14 16:45:46 -0500

    Fixed whitespace

--- vec2fx16.hpp
+++ vec2fx16.hpp
@@ -2,16 +2,16 @@
 
 struct vec2fx16 {
 
-    fixed16 x;
-    fixed16 y;
+	fixed16 x;
+	fixed16 y;
 
 	vec2fx16(): x(0),y(0) {}
-    vec2fx16(const fixed16 x,const fixed16 y): x(x),y(y) {}
+	vec2fx16(const fixed16 x,const fixed16 y): x(x),y(y) {}
 	vec2fx16(const vec2fx16& v): x(v.x),y(v.y) {}
 
 	void operator = (const vec2fx16& rhs) {
-        x = rhs.x;
-        y = rhs.y;
+		x = rhs.x;
+		y = rhs.y;
 	}
 	vec2fx16 operator + (const vec2fx16& rhs) { // 6502 clock time of ~88
 		return vec2fx16 {x+rhs.x, y+rhs.y};
@@ -26,18 +26,18 @@ struct vec2fx16 {
 		return vec2fx16 {x/scalarRHS,y/scalarRHS};
 	}
 
-    void operator += (const vec2fx16& rhs) {
-        (*this) = (*this)+rhs;
+	void operator += (const vec2fx16& rhs) {
+		(*this) = (*this)+rhs;
 	}
-    void operator -= (const vec2fx16& rhs) {
-        (*this) = (*this)+rhs;
+	void operator -= (const vec2fx16& rhs) {
+		(*this) = (*this)+rhs;
 	}
-    void operator *= (const fixed16& scalarRHS) {
-        (*this) = (*this)*scalarRHS;
+	void operator *= (const fixed16& scalarRHS) {
+		(*this) = (*this)*scalarRHS;
 	}
-    void operator /= (const fixed16& scalarRHS) {
-        (*this) = (*this)/scalarRHS;
+	void operator /= (const fixed16& scalarRHS) {
+		(*this) = (*this)/scalarRHS;
 	}
-    
+	
 
 };
